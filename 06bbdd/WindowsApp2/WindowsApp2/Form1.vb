a42a9b08bed09629b8097ab0e5abdc42424c6c80
j.estruch <jnestruch@gmail.com>
2021-02-05 18:53:27 +0100

    tema 6.bbdd - app 2

---
+++ 06bbdd/WindowsApp2/WindowsApp2/Form1.vb
@@ -0,0 +1,23 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim connection As SqlConnection = New SqlConnection()
+
+        connection.ConnectionString = "Data Source=localhost; Initial Catalog=prueba;Integrated Security=True"
+
+        connection.Open()
+
+        Dim adp As SqlDataAdapter = New SqlDataAdapter("select * from articulos", connection)
+
+        Dim ds As DataSet = New DataSet()
+
+        adp.Fill(ds)
+
+        DataGridView1.DataSource = ds.Tables(0)
+
+        connection.Close()
+
+    End Sub
+End Class
