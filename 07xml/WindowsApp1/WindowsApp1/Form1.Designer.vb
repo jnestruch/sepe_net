66468f5e54703d713250df3a89f07e9a1f8eb012
j.estruch <jnestruch@gmail.com>
2021-02-06 16:33:34 +0100

    tema 7.xml app teoria

---
+++ 07xml/WindowsApp1/WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,157 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ListBox3 = New System.Windows.Forms.ListBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.ListBox4 = New System.Windows.Forms.ListBox()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(25, 28)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 16
+        Me.ListBox1.Location = New System.Drawing.Point(48, 72)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(120, 84)
+        Me.ListBox1.TabIndex = 1
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.ItemHeight = 16
+        Me.ListBox2.Location = New System.Drawing.Point(203, 72)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(184, 84)
+        Me.ListBox2.TabIndex = 2
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(203, 28)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ListBox3
+        '
+        Me.ListBox3.FormattingEnabled = True
+        Me.ListBox3.ItemHeight = 16
+        Me.ListBox3.Location = New System.Drawing.Point(434, 72)
+        Me.ListBox3.Name = "ListBox3"
+        Me.ListBox3.Size = New System.Drawing.Size(320, 84)
+        Me.ListBox3.TabIndex = 4
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(434, 28)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 5
+        Me.Button3.Text = "Button3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Location = New System.Drawing.Point(48, 265)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.Size = New System.Drawing.Size(339, 250)
+        Me.WebBrowser1.TabIndex = 6
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(25, 222)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(210, 23)
+        Me.Button4.TabIndex = 7
+        Me.Button4.Text = "Mostrar archivo de autores"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'ListBox4
+        '
+        Me.ListBox4.FormattingEnabled = True
+        Me.ListBox4.ItemHeight = 16
+        Me.ListBox4.Location = New System.Drawing.Point(434, 265)
+        Me.ListBox4.Name = "ListBox4"
+        Me.ListBox4.Size = New System.Drawing.Size(193, 132)
+        Me.ListBox4.TabIndex = 8
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(434, 222)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(145, 23)
+        Me.Button5.TabIndex = 9
+        Me.Button5.Text = "Mostrar autores"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1002, 554)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.ListBox4)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.WebBrowser1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.ListBox3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.ListBox2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents ListBox2 As ListBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents ListBox3 As ListBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents WebBrowser1 As WebBrowser
+    Friend WithEvents Button4 As Button
+    Friend WithEvents ListBox4 As ListBox
+    Friend WithEvents Button5 As Button
+End Class
---
+++ 07xml/WindowsApp1/WindowsApp1/Form1.vb
@@ -0,0 +1,157 @@
+﻿Imports System.Xml
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Set the caption bar text of the form.
+
+        Me.Text = "Load XML"
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ListBox1().Items.Clear()
+
+        Dim xr As XmlReader = XmlReader.Create("movies.xml")
+
+        Do While xr.Read()
+
+            If xr.NodeType = XmlNodeType.Element AndAlso xr.Name = "movie" Then
+
+                ListBox1.Items.Add(xr.GetAttribute(0))
+
+            End If
+
+        Loop
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ListBox2().Items.Clear()
+
+        Dim xr As XmlReader = XmlReader.Create("movies.xml")
+
+        Do While xr.Read()
+
+            If xr.NodeType = XmlNodeType.Element AndAlso xr.Name = "type" Then
+
+                ListBox2.Items.Add(xr.ReadElementString)
+
+            Else
+
+                xr.Read()
+
+            End If
+
+        Loop
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        ListBox3().Items.Clear()
+
+        Dim xr As XmlReader = XmlReader.Create("movies.xml")
+
+        Do While xr.Read()
+
+            If xr.NodeType = XmlNodeType.Element AndAlso xr.Name = "description" Then
+
+                ListBox3.Items.Add(xr.ReadElementString)
+
+            Else
+
+                xr.Read()
+
+            End If
+
+        Loop
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim xws As XmlWriterSettings = New XmlWriterSettings()
+
+        xws.Indent = True
+        xws.NewLineOnAttributes = True
+
+        Dim xw As XmlWriter = XmlWriter.Create("authors.xml", xws)
+
+        xw.WriteStartDocument()
+        xw.WriteStartElement("Authors")
+
+        xw.WriteStartElement("author")
+        xw.WriteAttributeString("code", "1")
+        xw.WriteElementString("fname", "Carlos")
+        xw.WriteElementString("lname", "López")
+        xw.WriteEndElement()
+
+        xw.WriteStartElement("author")
+        xw.WriteAttributeString("code", "2")
+        xw.WriteElementString("fname", "Juan")
+        xw.WriteElementString("lname", "Pérez")
+        xw.WriteEndElement()
+
+        xw.WriteStartElement("author")
+        xw.WriteAttributeString("code", "3")
+        xw.WriteElementString("fname", "Isabel")
+        xw.WriteElementString("lname", "Gómez")
+        xw.WriteEndElement()
+
+        xw.WriteStartElement("author")
+        xw.WriteAttributeString("code", "4")
+        xw.WriteElementString("fname", "Ana")
+        xw.WriteElementString("lname", "Álvarez")
+        xw.WriteEndElement()
+
+        xw.WriteStartElement("author")
+        xw.WriteAttributeString("code", "5")
+        xw.WriteElementString("fname", "Sergio")
+        xw.WriteElementString("lname", "Valdecasas")
+        xw.WriteEndElement()
+        xw.WriteEndElement()
+
+        xw.WriteEndDocument()
+
+        xw.Flush()
+        xw.Close()
+
+        WebBrowser1.Url = New Uri(AppDomain.CurrentDomain.BaseDirectory + "authors.xml")
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        ListBox4.Items.Clear()
+
+        Dim xd As XmlDocument = New XmlDocument()
+        xd.Load("authors.xml")
+
+        Dim newAuthor As XmlElement = xd.CreateElement("author")
+
+        newAuthor.SetAttribute("code", "6")
+
+        Dim fn As XmlElement = xd.CreateElement("fname")
+
+        fn.InnerText = "Luisa"
+
+        newAuthor.AppendChild(fn)
+
+        Dim ln As XmlElement = xd.CreateElement("lname")
+
+        ln.InnerText = "Ruiz"
+
+        newAuthor.AppendChild(ln)
+
+        xd.DocumentElement.AppendChild(newAuthor)
+
+        Dim tr As XmlTextWriter = New XmlTextWriter("authors.xml", Nothing)
+
+        tr.Formatting = Formatting.Indented
+
+        xd.WriteContentTo(tr)
+
+        tr.Close()
+
+        Dim nl As XmlNodeList = xd.GetElementsByTagName("fname")
+
+        For Each node As XmlNode In nl
+
+            ListBox4.Items.Add(node.InnerText)
+
+        Next node
+
+    End Sub
+End Class
